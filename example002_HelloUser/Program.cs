da90cfa8eed7765c14451e4956e601f691dd1dd2
Dmitry <dmitry.grekov@mail.ru>
2023-01-16 18:57:06 +0200

    Hello User

---
+++ example002_HelloUser/Program.cs
@@ -0,0 +1,10 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Введите Ваше имя: ");
+        string username = Console.ReadLine();
+        Console.Write("Привет, ");
+        Console.Write(username);
+    }
+}
